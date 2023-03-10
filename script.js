4b903c3994cbf3ca3da180114dea983178f05215
NuremirKadyrbekov <118036760+NuremirKadyrbekov@users.noreply.github.com>
2023-03-10 16:38:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+ function save ()  {
+        let f = file1.files[0];
+        if (f) {
+            image1.src = URL.createObjectURL(f);
+            localStorage.setItem('myImage', image1.src);
+        }
+    }
+    
+    image1.src = localStorage.getItem('myImage')
+
+    const BtnDel = () =>{
+        localStorage.clear('myImage')
+    }
+    function refreshPage(){
+        window.location.reload();
+    } 
